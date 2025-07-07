e08547fd67ba78ca1086a8573dfc5b26f59d4434
Lukáš Dvorský <dvorskejlukas3333@gmail.com>
2025-07-07 13:36:10 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Header from './assets/Header'
+
+function App() {
+
+  return (
+    <main className='overflow-x-hidden'>
+      <Header/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/assets/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-screen h-screen bg-black'>
+        <p className='text-3xl text-white'>Lukáš Dvorský</p>
+        <span>Junior Developer</span>
+
+    </div>
+  )
+}
+
+export default Header
